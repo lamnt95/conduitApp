2f65b5896940880654aeea6f4d10ff99212d1236
Nguyen Tung Lam <tunglam20132208@gmail.com>
2019-11-12 10:36:19 +0700

    add event push

--- index.js
+++ index.js
@@ -3,15 +3,25 @@
  */
 
 import {Navigation} from 'react-native-navigation';
-import App from './App';
+import Home from './src/screens/Home';
+import About from './src/screens/About';
+import Profile from './src/screens/Profile';
 
-Navigation.registerComponent('navigation.playground.WelcomeScreen', () => App);
+Navigation.registerComponent('navigation.playground.Home', () => Home);
+Navigation.registerComponent('navigation.playground.About', () => About);
+Navigation.registerComponent('navigation.playground.Profile', () => Profile);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
-      component: {
-        name: 'navigation.playground.WelcomeScreen',
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'navigation.playground.Home',
+            },
+          },
+        ],
       },
     },
   });
---
+++ src/screens/Profile.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {Navigation} from 'react-native-navigation';
+
+const styles = StyleSheet.create({
+  btn: {
+    width: 200,
+    padding: 10,
+    marginTop: 10,
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+});
+
+export default class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    Navigation.events().bindComponent(this);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Profile</Text>
+        <TouchableOpacity style={styles.btn}>
+          <Text>Go to About</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn}>
+          <Text>Go to Profile</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
